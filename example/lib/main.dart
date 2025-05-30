4e8297d2e30d497722bae9b040805e72d3df87f0
Mat Nuckowski <mat@nuckowski.com>
2025-05-30 18:59:06 +1200

    Fixed all linter warnings in the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'ui/home_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- example/lib/ui/example_page.dart
+++ example/lib/ui/example_page.dart
@@ -9,43 +9,44 @@ class ExampleViewModel {
   final double value;
   final InnerWidget? innerWidget;
 
-  ExampleViewModel(
-      {required this.pageColors,
-      required this.appearance,
-      this.min = 0,
-      this.max = 100,
-      this.value = 50,
-      this.innerWidget});
+  ExampleViewModel({
+    required this.pageColors,
+    required this.appearance,
+    this.min = 0,
+    this.max = 100,
+    this.value = 50,
+    this.innerWidget,
+  });
 }
 
 class ExamplePage extends StatelessWidget {
   final ExampleViewModel viewModel;
-  const ExamplePage({
-    Key? key,
-    required this.viewModel,
-  }) : super(key: key);
+  const ExamplePage({super.key, required this.viewModel});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: viewModel.pageColors,
-                begin: Alignment.bottomLeft,
-                end: Alignment.topRight,
-                tileMode: TileMode.clamp)),
+          gradient: LinearGradient(
+            colors: viewModel.pageColors,
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+            tileMode: TileMode.clamp,
+          ),
+        ),
         child: SafeArea(
           child: Center(
-              child: SleekCircularSlider(
-            onChangeStart: (double value) {},
-            onChangeEnd: (double value) {},
-            innerWidget: viewModel.innerWidget,
-            appearance: viewModel.appearance,
-            min: viewModel.min,
-            max: viewModel.max,
-            initialValue: viewModel.value,
-          )),
+            child: SleekCircularSlider(
+              onChangeStart: (double value) {},
+              onChangeEnd: (double value) {},
+              innerWidget: viewModel.innerWidget,
+              appearance: viewModel.appearance,
+              min: viewModel.min,
+              max: viewModel.max,
+              initialValue: viewModel.value,
+            ),
+          ),
         ),
       ),
     );
